1e9354abdc09d3c6473f5229939667bbbaf34915
rafa-alves-15 <rafaelalves.15@outlook.com>
2021-07-14 22:50:19 -0300

    Completed lab

--- index.js
+++ index.js
@@ -12,16 +12,39 @@ mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
+  .then(async () => {
+    const result = await Recipe.create({
+      title: "Macarrão",
+      level: "Easy Peasy",
+      ingredients: ["Macarrão", "Àgua", "Molho de Tomate", "Carne Moida"],
+      cuisine: "Italiano",
+      dihType: "main_course",
+      image:
+        "https://img.itdg.com.br/tdg/images/recipes/000/302/674/326343/326343_original.jpg?mode=crop&width=710&height=400",
+      duration: 30,
+      creator: "Chef Italiano",
+    });
+    const manyResult = await Recipe.insertMany(data);
+
+    const updatedRecipe = await Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { $set: { duration: 100 } },
+      { new: true }
+    );
+
+    const deletedRecipe = await Recipe.deleteOne({
+      title: "Carrot Cake",
+    });
+    mongoose.connection.close(); 
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
